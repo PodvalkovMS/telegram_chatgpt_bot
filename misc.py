cf10dec1d6fdd9d5fcd2f6587c85c65d824bce61
PodvalkovMS <59251137+PodvalkovMS@users.noreply.github.com>
2023-11-14 09:08:54 +0000

    Telegram bot chat gpt

--- misc.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Bot, Dispatcher
-from aiogram.enums.parse_mode import ParseMode
-from aiogram.fsm.storage.memory import MemoryStorage
-import os
-
-bot = Bot(token=os.environ['TOKEN'], parse_mode=ParseMode.HTML)
-dp = Dispatcher(storage=MemoryStorage())
